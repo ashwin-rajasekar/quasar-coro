fcc710018b68d12950394b3243d8526cf8398b50
Ashwin Rajasekar <ashraj101010@gmail.com>
2025-01-12 11:24:04 -0600

    dont inherit from std awaiters; better aggregate list init

--- include/quasar/coro/await.hpp
+++ include/quasar/coro/await.hpp
@@ -7,36 +7,46 @@ namespace quasar::coro::await {
 	template<class Coro> struct delegate {
 		Coro task;
 
-		bool await_ready() const noexcept { return task.done(); }
+		constexpr bool await_ready() const noexcept { return task.done(); }
 
-		std::coroutine_handle<void> await_suspend(auto caller) const noexcept {
+		constexpr std::coroutine_handle<void> await_suspend(auto caller) const noexcept {
 			task.promise().set_continuation(caller);
 			return static_cast<std::coroutine_handle<void>>(task);
 		}
 
-		decltype(auto) await_resume() const noexcept(!requires{ task.promise().rethrow(); }) {
+		constexpr decltype(auto) await_resume() const noexcept(!requires{ task.promise().rethrow(); }) {
 			if constexpr(requires{ task.promise().rethrow(); }){ task.promise().rethrow(); }
 			if constexpr(requires{ task.promise().get_result(); }){ return task.promise().get_result(); }
 		}
 	};
 
-	struct handoff : std::suspend_always {
+	struct handoff {
 		std::coroutine_handle<void> task;
 
-		bool await_ready() const noexcept { return !task; }
+		constexpr bool await_ready() const noexcept { return !task; }
 
-		std::coroutine_handle<void> await_suspend(auto) const noexcept { return task; }
+		constexpr std::coroutine_handle<void> await_suspend(auto) const noexcept { return task; }
+
+		constexpr void await_resume() const noexcept {}
 	};
 
-	template<class Func> struct callback : std::suspend_always {
+	template<class Func> struct callback {
 		Func func;
 
-		auto await_suspend(auto coro) const noexcept { return func(coro); }
+		constexpr bool await_ready() const noexcept { return false; }
+
+		constexpr auto await_suspend(auto coro) const noexcept { return func(coro); }
+
+		constexpr void await_resume() const noexcept {}
 	};
 
-	template<class T> struct fetch : std::suspend_never {
+	template<class T> struct fetch {
 		T value;
 
-		T await_resume() noexcept { return std::forward<T>(value); }
+		constexpr bool await_ready() const noexcept { return true; }
+
+		constexpr void await_suspend() const noexcept {}
+
+		constexpr T await_resume() noexcept { return std::forward<T>(value); }
 	};
 }
