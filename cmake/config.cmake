3d2cb48b85df0f4912c0692e7d832a28ec00d321
Ashwin Rajasekar <ashraj101010@gmail.com>
2025-01-23 18:08:37 -0600

    impropve install script with more project-independence

--- cmake/config.cmake
+++ cmake/config.cmake
@@ -1,1 +1,1 @@
-include(${CMAKE_CURRENT_LIST_DIR}/quasar-coro-targets.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/@PROJECT_NAME@-targets.cmake)
--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,25 +1,28 @@
 include(GNUInstallDirs)
 include(CMakePackageConfigHelpers)
 
+# Set Architecture-Independence flag as appropriate
 if(${QUASAR_ARCH_INDEPENDENT})
 	set(QUASAR_ARCH_INDEPENDENT ARCH_INDEPENDENT)
 else()
 	set(QUASAR_ARCH_INDEPENDENT)
 endif()
 
+# Generate Package Config
 configure_package_config_file(
 	${PROJECT_SOURCE_DIR}/cmake/config.cmake
 	${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config.cmake
 	INSTALL_DESTINATION ${CMAKE_INSTALL_DATADIR}/cmake/${PROJECT_NAME}
 )
 
+# Generate Package Version Config
 write_basic_package_version_file(
 	${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config-version.cmake
-	VERSION ${PROJECT_VERSION}
 	COMPATIBILITY SameMinorVersion
 	${QUASAR_ARCH_INDEPENDENT}
 )
 
+# Install Package Config & Version Config
 install(
 	FILES
 		${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config.cmake
@@ -27,6 +30,7 @@ install(
 	DESTINATION ${CMAKE_INSTALL_DATADIR}/cmake/${PROJECT_NAME}
 )
 
+# Install Licenses
 install(
 	FILES
 		${PROJECT_SOURCE_DIR}/COPYING
@@ -34,6 +38,7 @@ install(
 	DESTINATION ${CMAKE_INSTALL_DOCDIR}
 )
 
+# Create Installation Target Set
 install(
 	TARGETS ${QUASAR_INSTALL_TARGETS}
 	EXPORT ${PROJECT_NAME}-targets
@@ -47,12 +52,14 @@ install(
 	INCLUDES      DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
 )
 
+# Install Targets & Set Namespacing
 install(
 	EXPORT ${PROJECT_NAME}-targets
 	NAMESPACE quasar::
 	DESTINATION ${CMAKE_INSTALL_DATADIR}/cmake/${PROJECT_NAME}
 )
 
+# Install Interface Headers
 install(
 	DIRECTORY ${PROJECT_SOURCE_DIR}/include/
 	DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
