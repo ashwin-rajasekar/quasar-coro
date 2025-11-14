0edf55ced9b63963664e8e727a70b2070fc99672
Ashwin Rajasekar <ashraj101010@gmail.com>
2025-11-14 19:48:10 +0000

    simplify installation scripts

--- cmake/install.cmake
+++ cmake/install.cmake
@@ -1,32 +1,16 @@
 include(GNUInstallDirs)
 include(CMakePackageConfigHelpers)
 
-# Set Architecture-Independence flag as appropriate
-if(${QUASAR_ARCH_INDEPENDENT})
-	set(QUASAR_ARCH_INDEPENDENT ARCH_INDEPENDENT)
-else()
-	set(QUASAR_ARCH_INDEPENDENT)
-endif()
-
-# Generate Package Config
-configure_package_config_file(
-	${PROJECT_SOURCE_DIR}/cmake/config.cmake
-	${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config.cmake
-	INSTALL_DESTINATION ${CMAKE_INSTALL_DATADIR}/cmake/${PROJECT_NAME}
-)
-
 # Generate Package Version Config
 write_basic_package_version_file(
 	${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config-version.cmake
 	COMPATIBILITY SameMinorVersion
-	${QUASAR_ARCH_INDEPENDENT}
+	ARCH_INDEPENDENT
 )
 
 # Install Package Config & Version Config
 install(
-	FILES
-		${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config.cmake
-		${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config-version.cmake
+	FILES ${PROJECT_BINARY_DIR}/${PROJECT_NAME}-config-version.cmake
 	DESTINATION ${CMAKE_INSTALL_DATADIR}/cmake/${PROJECT_NAME}
 )
 
@@ -35,32 +19,19 @@ install(
 	FILES
 		${PROJECT_SOURCE_DIR}/COPYING
 		${PROJECT_SOURCE_DIR}/COPYING.LESSER
-	DESTINATION ${CMAKE_INSTALL_DOCDIR}
+	TYPE DOC
 )
 
 # Create Installation Target Set
 install(
-	TARGETS ${QUASAR_INSTALL_TARGETS}
-	EXPORT ${PROJECT_NAME}-targets
-	RUNTIME_DEPENDENCIES
-	BUNDLE        DESTINATION ${CMAKE_INSTALL_BINDIR}
-	RUNTIME       DESTINATION ${CMAKE_INSTALL_BINDIR}
-	LIBRARY       DESTINATION ${CMAKE_INSTALL_LIBDIR}
-	ARCHIVE       DESTINATION ${CMAKE_INSTALL_LIBDIR}
-	FRAMEWORK     DESTINATION ${CMAKE_INSTALL_LIBDIR}
-	PUBLIC_HEADER DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
-	INCLUDES      DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
+	TARGETS coro
+	EXPORT ${PROJECT_NAME}-config
+	FILE_SET HEADERS
 )
 
 # Install Targets & Set Namespacing
 install(
-	EXPORT ${PROJECT_NAME}-targets
+	EXPORT ${PROJECT_NAME}-config
 	NAMESPACE quasar::
 	DESTINATION ${CMAKE_INSTALL_DATADIR}/cmake/${PROJECT_NAME}
-)
-
-# Install Interface Headers
-install(
-	DIRECTORY ${PROJECT_SOURCE_DIR}/include/
-	DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}
 )
